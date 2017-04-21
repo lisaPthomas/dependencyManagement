c105e15b2f396bfe34d50f7e48a0ebf9509561f8
Lisa Thomas <lisathomas@Lisas-MacBook-Pro.local>
2017-04-21 02:45:37 -0700

    list of dependencies with no duplicates

--- DependencyOrder.js
+++ DependencyOrder.js
@@ -1,20 +1,50 @@
 function arrayTest() {
-  //each string contains one dependency: 'namePackage: dependency'
-  //valid input in Javascript
-  var packageArray = ['KittenService: CamelCaser', 'Leetmeme: Cyberportal', 'Package: Toastr'];
+    //each string contains one dependency: 'namePackage: dependency'
+    //valid input in Javascript
+    var packageArray = [
+        'KittenService: ',
+        'Leetmeme: Cyberportal',
+        'Cyberportal: Ice',
+        'CamelCaser: KittenService',
+        'FraudStream: Leetmeme',
+        'Ice: '
+    ];
 
-  //store output list into array..need to modify later
-  var orderList = []; //reverses array indexes
-  var storeNewArray = []; //stores new array elements
+    //store output list into array..need to modify later
+    var orderList = []; //reverses array indexes
+    var storeNewArray = []; //stores new array elements
 
-  //for loop iterates through packages and orders them so dependency comes first
-  for(var i=0; i<packageArray.length; i++) {
-    orderList = packageArray[i].split(": ").reverse();
-    storeNewArray.push(orderList[0], orderList[1]);
-    };
+    for (var i = 0; i < packageArray.length; i++) {
 
-    var list = storeNewArray.join(", ");
-    return (list);
-  }
+        orderList = packageArray[i].split(": ").reverse();
+
+        if (orderList[0] === "") {
+            // storeNewArray.push(orderList[1]);
+            var packageName = orderList[1];
+            storeNewArray.splice(0, 0, orderList[1]);
+        } else {
+
+            orderList.forEach(function(value, index, array) {
+                if (storeNewArray.includes(value)) {
+                    console.log(value);
+                } else {
+                    storeNewArray.push(value);
+                }
+            })
+        }
+    }
+
+    function finalList(storeNewArray) {
+        var noDups = [];
+        for (var i = 0; i < storeNewArray.length; i++) {
+            if (noDups.indexOf(storeNewArray[i]) == -1) {
+                noDups.push(storeNewArray[i]);
+            }
+        }
+        return noDups.join(", ");
+    }
+
+    return (finalList(storeNewArray));
+}
 
 arrayTest();
--- test/spec/DependencyOrderSpec.js
+++ test/spec/DependencyOrderSpec.js
@@ -1,7 +1,7 @@
 describe("dependencyOrder", function(){
 
   it('should order the array with dependencies preceding package name', function(){
-    expect(arrayTest()).toEqual('CamelCaser, KittenService, Cyberportal, Leetmeme, Toastr, Package');
+    expect(arrayTest()).toEqual('Ice, KittenService, Cyberportal, Leetmeme, CamelCaser, FraudStream');
   })
 
 });
