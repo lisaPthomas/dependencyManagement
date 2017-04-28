dd424390d54bb0bcb40cbfe3a01aed5d6bb92dd7
Lisa Thomas <lisathomas@Lisas-MacBook-Pro.local>
2017-04-27 19:56:33 -0700

    puts new package/dependency at end of array

--- DependencyOrder.js
+++ DependencyOrder.js
@@ -1,53 +1,65 @@
-function noDependency(array, item) {
-    var orderList = item.split(": ");
-    if (orderList[1] === "") {
-        array.splice(0, 0, orderList[0]);
-    }
-    return array;
+function orderList(packageArray, item) {
+    var orderList = item.split(": ").reverse();
+
+    if (orderList[0] === "") {
+        packageArray.splice(0, 0, orderList[1]);
+    } else {
+    orderList.forEach(function(value, index){
+      if(packageArray.includes(value)){
+        console.log(packageArray);
+      } else {
+        packageArray.push(value);
+      }
+    })
+  }
+
+    return packageArray;
+
 }
+orderList();
 
-function arrayTest(packageArray) {
-
-    var orderList = []; //container for storing new values
-    var storeNewArray = []; //stores new array elements in correct order
-
-    //iterates through the array pushes values into storeNewArray
-    for (var i = 0; i < packageArray.length; i++) {
-
-        orderList = packageArray[i].split(": ").reverse();
-
-        //checks if dependency is an empty string,
-        //if dependency exists iterates through the pair and pushes new item to array
-
-        if (orderList[0] === "") {
-            var packageName = orderList[1];
-            storeNewArray.splice(0, 0, orderList[1]);
-        } else {
-            orderList.forEach(function(value, index, array) {
-                if (storeNewArray.includes(value)) {
-                    console.log(value);
-                } else {
-                    storeNewArray.push(value);
-                }
-            })
-        }
-    }
-
-    //function iterates through storeNewArray and checks to see if there are duplicates in array
-    function finalList(storeNewArray) {
-        var noDups = [];
-        for (var i = 0; i < storeNewArray.length; i++) {
-            if (noDups.indexOf(storeNewArray[i]) == -1) {
-                noDups.push(storeNewArray[i]);
-            }
-        }
-        return noDups.join(", ");
-    }
-
-    return (finalList(storeNewArray));
-}
-
-arrayTest(packageArray);
+// function arrayTest(packageArray) {
+//
+//     var orderList = []; //container for storing new values
+//     var storeNewArray = []; //stores new array elements in correct order
+//
+//     //iterates through the array pushes values into storeNewArray
+//     for (var i = 0; i < packageArray.length; i++) {
+//
+//         orderList = packageArray[i].split(": ").reverse();
+//
+//         //checks if dependency is an empty string,
+//         //if dependency exists iterates through the pair and pushes new item to array
+//
+//         if (orderList[0] === "") {
+//             var packageName = orderList[1];
+//             storeNewArray.splice(0, 0, orderList[1]);
+//         } else {
+//             orderList.forEach(function(value, index, array) {
+//                 if (storeNewArray.includes(value)) {
+//                     console.log(value);
+//                 } else {
+//                     storeNewArray.push(value);
+//                 }
+//             })
+//         }
+//     }
+//
+//     //function iterates through storeNewArray and checks to see if there are duplicates in array
+//     function finalList(storeNewArray) {
+//         var noDups = [];
+//         for (var i = 0; i < storeNewArray.length; i++) {
+//             if (noDups.indexOf(storeNewArray[i]) == -1) {
+//                 noDups.push(storeNewArray[i]);
+//             }
+//         }
+//         return noDups.join(", ");
+//     }
+//
+//     return (finalList(storeNewArray));
+// }
+//
+// arrayTest(packageArray);
 
 // var stack = [];
 // var visited = [];
--- test/spec/DependencyOrderSpec.js
+++ test/spec/DependencyOrderSpec.js
@@ -11,24 +11,29 @@
 describe("dependencyOrder", function() {
     //first test to put all items without a dependency at beginning of storeNewArray
     it('should put item WITHOUT dependency at beginning of array', function() {
-        var array = ['foo'];
+        var packageArray = ['foo'];
         var item = 'bar: ';
-        expect(noDependency(array, item)).toEqual(['bar', 'foo'])
+        expect(orderList(packageArray, item)).toEqual(['bar', 'foo'])
+    })
+
+    it('should put package and dependency at the end of array if neither exists in visited array', function() {
+       var packageArray = ['foo'];
+       var item = 'bar: baz';
+       expect(orderList(packageArray, item)).toEqual(['foo', 'baz', 'bar'])
     })
 
     //last test
-    it('should order the array with dependencies preceding package name', function() {
-
-        var packageArray = [
-            'KittenService: ',
-            'Leetmeme: Cyberportal',
-            'Cyberportal: Ice',
-            'CamelCaser: KittenService',
-            'FraudStream: Leetmeme',
-            'Toaster: Mocha',
-            'Ice: '
-        ];
-        expect(arrayTest(packageArray)).toEqual('Ice, KittenService, Cyberportal, Leetmeme, CamelCaser, FraudStream, Mocha, Toaster');
-    })
+    // it('should order the array with dependencies preceding package name', function() {
+    //     var packageArray = [
+    //         'KittenService: ',
+    //         'Leetmeme: Cyberportal',
+    //         'Cyberportal: Ice',
+    //         'CamelCaser: KittenService',
+    //         'FraudStream: Leetmeme',
+    //         'Toaster: Mocha',
+    //         'Ice: '
+    //     ];
+    //     expect(arrayTest(packageArray)).toEqual('Ice, KittenService, Cyberportal, Leetmeme, CamelCaser, FraudStream, Mocha, Toaster');
+    // })
 
 });
