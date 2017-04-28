fa74c8a14df1c6461b0c37e255025f68ee771496
Lisa Thomas <lisathomas@Lisas-MacBook-Pro.local>
2017-04-28 13:51:46 -0700

    need to pass fifth and sixth test

--- DependencyOrder.js
+++ DependencyOrder.js
@@ -1,98 +1,38 @@
-function orderItem(stackArray, arrayInput, visited) {
-    for (var i = 0; i < arrayInput.length; i++) {
-        var orderList = arrayInput[i].split(": ");
-        var package = orderList[0];
-        var dependency = orderList[1];
-    
-        if(visited.includes(orderList[0])){
-          continue;
-        } else {
-          topSortUtil(package, dependency, stackArray, visited)
-        }
+function orderItem(packageArray, item) {
+  var dataArray = []; //New Array with arrays in order to have access to data structure
 
-function topSortUtil(package, dependency, stackArray, visited) {
-        if (orderList[1] === "") {
+    for (var i = 0; i < item.length; i++) {
+        var orderList = item[i].split(": ").reverse();
 
-                stackArray.splice(0, 0, orderList[0]);
+        dataArray.push(orderList);
+
+        if (orderList[0] === "") {
+            //needed to filter out existing packages
+            if (packageArray.includes(orderList[1])) {
+                continue;
+            } else {
+                packageArray.splice(0, 0, orderList[1]);
+            }
 
         } else {
             orderList.forEach(function(value, index) {
-                if (stackArray.includes(value)) {
+                if (packageArray.includes(value)) {
                     return;
                 } else {
-                    stackArray.push(value);
+                    packageArray.push(value);
                 }
             })
         }
+
+
+          //need new function to iterate through dataArray in order to top sort dependencies
+          //need visited array to test if dependency has been visited
+          //once dependency has no other dependencies need to add to final packageArray
+          //???How to explore dependencies in an dataArray and grab their child nodes...
+          //How to check for cyclical dependencies and throw an error
+
     }
-  }
-    return visited;
+    return packageArray.join(", ");
+
 }
 orderItem();
-
-// function arrayTest(packageArray) {
-//
-//     var orderList = []; //container for storing new values
-//     var storeNewArray = []; //stores new array elements in correct order
-//
-//     //iterates through the array pushes values into storeNewArray
-//     for (var i = 0; i < packageArray.length; i++) {
-//
-//         orderList = packageArray[i].split(": ").reverse();
-//
-//         //checks if dependency is an empty string,
-//         //if dependency exists iterates through the pair and pushes new item to array
-//
-//         if (orderList[0] === "") {
-//             var packageName = orderList[1];
-//             storeNewArray.splice(0, 0, orderList[1]);
-//         } else {
-//             orderList.forEach(function(value, index, array) {
-//                 if (storeNewArray.includes(value)) {
-//                     console.log(value);
-//                 } else {
-//                     storeNewArray.push(value);
-//                 }
-//             })
-//         }
-//     }
-//
-//     //function iterates through storeNewArray and checks to see if there are duplicates in array
-//     function finalList(storeNewArray) {
-//         var noDups = [];
-//         for (var i = 0; i < storeNewArray.length; i++) {
-//             if (noDups.indexOf(storeNewArray[i]) == -1) {
-//                 noDups.push(storeNewArray[i]);
-//             }
-//         }
-//         return noDups.join(", ");
-//     }
-//
-//     return (finalList(storeNewArray));
-// }
-//
-// arrayTest(packageArray);
-
-// var stack = [];
-// var visited = [];
-// var tempArray = [];
-//
-// for (var i = 0; i < packageArray.length; i++) {
-//     var pair = packageArray[i].split(": ");
-//     // var pos0 = pair[0];
-//     // var pos1 = pair[1];
-//
-//     tempArray.push(pair[0],pair[1]);
-// };
-//
-// console.log('tempArray: ',tempArray);
-//
-// for (var j = 0; j < tempArray.length; j++) {
-//
-//     if (visited.includes(tempArray[j])) {
-//         continue
-//     } else {
-//         visited.push(tempArray[j]);
-//     }
-// }
-// console.log(visited);
--- test/spec/DependencyOrderSpec.js
+++ test/spec/DependencyOrderSpec.js
@@ -1,57 +1,59 @@
 describe("dependencyOrder", function() {
     // //first test to put all items without a dependency at beginning of storeNewArray
-    // it('should put item WITHOUT dependency at beginning of array', function() {
-    //     var packageArray = ['foo'];
-    //     var item = ['bar: '];
-    //     expect(orderItem(packageArray, item)).toEqual(['bar', 'foo']);
-    // })
-    //
-    // //second test to place package and dependency at end of array
-    // it('should put package and dependency at the end of array if neither exists in visited array', function() {
-    //     var packageArray = ['foo'];
-    //     var item = ['bar: baz'];
-    //     expect(orderItem(packageArray, item)).toEqual(['foo', 'baz', 'bar']);
-    // })
-    //
-    // //third test to filter out existing packages/dependencies
-    // it('should filter out package and only put dependency at end of array if package exists in the array', function() {
-    //     var packageArray = ['foo'];
-    //     var item = ['bar: foo'];
-    //     expect(orderItem(packageArray, item)).toEqual(['foo', 'bar']);
-    // })
-    //
-    // //fourth test to iterate through multiple values
-    // it('should filter out package and only put dependency at end of array if package exists in the array', function() {
-    //     var stackArray = ['foo'];
-    //     var arrayInput = ['KittenService: CamelCaser', 'CamelCaser: '];
-    //     expect(orderItem(stackArray, arrayInput)).toEqual(['foo', 'CamelCaser', 'KittenService']);
-    // })
-    //fifth test, should be only passing test, checks if packages have been visited yet(start of sort)
-    it('should iterate through arrayInput and add to visited array', function() {
-        var stackArray=[];
-        var visited=[];
-        var arrayInput = [
+    it('should put item WITHOUT dependency at beginning of array', function() {
+        var packageArray = ['foo'];
+        var item = ['bar: '];
+        expect(orderItem(packageArray, item)).toEqual('bar, foo');
+    })
+
+    //second test to place package and dependency at end of array
+    it('should put package and dependency at the end of array if neither exists in visited array', function() {
+       var packageArray = ['foo'];
+       var item = ['bar: baz'];
+       expect(orderItem(packageArray, item)).toEqual('foo, baz, bar');
+    })
+
+    //third test to filter out existing packages/dependencies
+    it('should filter out package and only put dependency at end of array if package exists in the array', function(){
+      var packageArray = ['foo'];
+      var item = ['bar: foo'];
+      expect(orderItem(packageArray, item)).toEqual('foo, bar');
+    })
+
+    //fourth test to iterate through multiple values
+    it('should filter out package and only put dependency at end of array if package exists in the array', function(){
+      var packageArray = [];
+      var item = ['KittenService: CamelCaser', 'CamelCaser: '];
+      expect(orderItem(packageArray, item)).toEqual('CamelCaser, KittenService');
+    })
+
+    //fifth test
+    it('should order the array with dependencies preceding package name', function() {
+        var packageArray = [];
+        var item = [
             'KittenService: ',
             'Leetmeme: Cyberportal',
             'Cyberportal: Ice',
-            'Fraudstream: Leetmeme',
+            'CamelCaser: KittenService',
+            'FraudStream: Leetmeme',
+            'Toaster: Mocha',
+            'Ice: '
         ];
-        expect(orderItem(stackArray, arrayInput, visited)).toEqual(['KittenService', 'Ice', 'Cyberportal', 'Leetmeme', 'Fraudstream']);
+        expect(orderItem(packageArray, item)).toEqual('Ice, KittenService, Cyberportal, Leetmeme, CamelCaser, FraudStream, Mocha, Toaster');
     })
 
-    // //last test
-    // it('should order the array with dependencies preceding package name', function() {
-    //     var packageArray = ['foo'];
-    //     var item = [
-    //         'KittenService: ',
-    //         'Leetmeme: Cyberportal',
-    //         'Cyberportal: Ice',
-    //         'CamelCaser: KittenService',
-    //         'FraudStream: Leetmeme',
-    //         'Toaster: Mocha',
-    //         'Ice: '
-    //     ];
-    //     expect(orderItem(packageArray, item)).toEqual('Ice, KittenService, Cyberportal, Leetmeme, CamelCaser, FraudStream, Mocha, Toaster');
-    // })
+    //sixth test needed to test cyclical dependencies
+    it('should throw an error(contains cycles)', function() {
+        var packageArray = [];
+        var item = [
+            'KittenService: ',
+            'Leetmeme: Cyberportal',
+            'Cyberportal: Ice',
+            'CamelCaser: KittenService',
+            'FraudStream: ',
+            'Ice: Leetmeme',
+        ];
+        expect(orderItem(packageArray, item)).toEqual('Error Message');
+    })
 
 });
